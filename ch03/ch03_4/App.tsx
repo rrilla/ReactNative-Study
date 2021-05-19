fbd40e3925b949da3d4e6cbfe286bc18ab61de49
rrilla <rrilla@naver.com>
2021-05-19 21:21:30 +0900

    컴포넌트 스타일링 - 컴포넌트 배치관련 스타일 속성

---
+++ ch03/ch03_4/App.tsx
@@ -0,0 +1,34 @@
+// 화면에 뜬 효과 주기
+import React from 'react'
+import {Platform, StyleSheet, SafeAreaView, View, Alert} from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import {Colors} from 'react-native-paper'
+import TopBar from './src/screens/TopBar'
+import Content from './src/screens/Content'
+import BottomBar from './src/screens/BottomBar'
+
+export default function App() {
+  const iconPressed = () => Alert.alert('Icon pressed.')
+
+  return (
+    <SafeAreaView style={styles.flex}>
+      <TopBar />
+      <Content />
+      <BottomBar />
+      <View style={[styles.absoluteView]}>
+        <Icon name="feather" size={50} color="white" onPress={iconPressed} />
+      </View>
+    </SafeAreaView>
+  )
+}
+const styles = StyleSheet.create({
+  flex: {flex: 1, backgroundColor: Colors.lightBlue100},
+  absoluteView: {
+    backgroundColor: Colors.purple900,
+    position: 'absolute',
+    right: 30,
+    bottom: Platform.select({ios: 100, android: 80}),
+    padding: 10,
+    borderRadius: 35,
+  },
+})
