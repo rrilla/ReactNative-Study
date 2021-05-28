3c70e3bdb24c22f10ea6b19df9c52ceb2c6507b0
rrilla <rrilla@naver.com>
2021-05-28 22:02:03 +0900

    콘텍스트, ref속성 - 콘텍스트 이해

--- ch05/ch05_1/App.tsx
+++ ch05/ch05_1/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react'
 import {SafeAreaView, StyleSheet} from 'react-native'
-import {AppearanceProvider} from 'react-native-appearance'
+import {AppearanceProvider, useColorScheme} from 'react-native-appearance'
+import {
+  DarkTheme,
+  DefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper'
 import MainNavigator from './src/screens/MainNavigator'
 
 export default function App() {
+  const scheme = useColorScheme()
+  const [theme, setTheme] = useState(
+    scheme == 'dark' ? DarkTheme : DefaultTheme,
+  )
+  const toggleTheme = useCallback(
+    () => setTheme(theme => (theme.dark ? DefaultTheme : DarkTheme)),
+    [],
+  )
+  console.log(scheme)
   return (
     <AppearanceProvider>
-      <SafeAreaView style={[styles.safeAreaView]}>
-        <MainNavigator />
-      </SafeAreaView>
+      <PaperProvider theme={DarkTheme}>
+        <SafeAreaView style={[styles.safeAreaView]}>
+          <MainNavigator />
+        </SafeAreaView>
+      </PaperProvider>
     </AppearanceProvider>
   )
 }
