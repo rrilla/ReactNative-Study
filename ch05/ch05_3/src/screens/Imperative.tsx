eb201e3792a084291060cb91e9ff4125255136d0
rrilla <rrilla@naver.com>
2021-05-30 17:58:24 +0900

    콘텍스트, ref 속성 - useImperativeHandle훅

--- ch05/ch05_3/src/screens/Imperative.tsx
+++ ch05/ch05_3/src/screens/Imperative.tsx
@@ -1,17 +1,71 @@
-import React from 'react'
-import {StyleSheet, View, Text} from 'react-native'
-import {Colors} from 'react-native-paper'
-import * as D from '../data'
+import React, {useRef, useCallback, useState, useMemo} from 'react';
+// prettier-ignore
+import {StyleSheet, Switch, TextInput as RNTextInput, Keyboard} from 'react-native'
+import {Text, TextInput, View} from '../theme/paper';
+import {useTheme} from 'react-native-paper';
+import {useToggleTheme, AutoFocusProvider, useAutoFocus} from '../contexts';
+import * as D from '../data';
+import ImperativeTextInput from './ImperativeTextInput';
+import type {TextInputMethods} from './ImperativeTextInput';
 
-const title = 'CopyMe'
-export default function CopyMe() {
+export default function AutoFocus() {
+  const [person, setPerson] = useState<D.IPerson>(D.createRandomPerson());
+  const {dark} = useTheme();
+  const toggleTheme = useToggleTheme();
+  const autoFocus = useAutoFocus();
+
+  const methodsRef = useRef<TextInputMethods | null>(null);
+  const setFocus = useCallback(() => methodsRef.current?.focus(), []);
+  const dismissKeyboard = useCallback(() => {
+    methodsRef.current?.dismiss();
+  }, []);
+
   return (
     <View style={[styles.view]}>
-      <Text style={[styles.text]}>{title}</Text>
+      <View style={[styles.topBar]}>
+        <Text style={[styles.textButton]} onPress={setFocus}>
+          focus
+        </Text>
+        <Text style={[styles.textButton]} onPress={dismissKeyboard}>
+          dismiss keyboard
+        </Text>
+        <View style={{flex: 1}} />
+        <Switch value={dark} onValueChange={toggleTheme} />
+      </View>
+      <AutoFocusProvider contentContainerStyle={[styles.flex]}>
+        <View style={[styles.flex]} />
+        <View style={[styles.textView]}>
+          <Text style={[styles.text]}>email</Text>
+          <ImperativeTextInput
+            style={[styles.textInput]}
+            onFocus={autoFocus}
+            value={person.email}
+            placeholder="enter your email"
+            onChangeText={email => setPerson(person => ({...person, email}))}
+          />
+        </View>
+        <View style={[styles.textView]}>
+          <Text style={[styles.text]}>name</Text>
+          <ImperativeTextInput
+            ref={methodsRef}
+            style={[styles.textInput]}
+            onFocus={autoFocus}
+            value={person.name}
+            placeholder="enter your name"
+            onChangeText={name => setPerson(person => ({...person, name}))}
+          />
+        </View>
+      </AutoFocusProvider>
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
-  view: {flex: 1, padding: 5, backgroundColor: Colors.blue900},
-  text: {fontSize: 20, color: 'white'},
-})
+  view: {flex: 1},
+  topBar: {flexDirection: 'row', padding: 5},
+  textButton: {marginLeft: 10, fontSize: 20},
+  keyboardAvoidingView: {flex: 1, padding: 10},
+  textView: {padding: 5},
+  text: {fontSize: 24},
+  textInput: {fontSize: 24, borderWidth: 1, borderRadius: 5},
+  flex: {flex: 1},
+});
--- ch05/ch05_3/src/theme/paper/TextInput.tsx
+++ ch05/ch05_3/src/theme/paper/TextInput.tsx
@@ -1,25 +1,14 @@
 // 컴파일 오류가 있는 초기 모습
-// import React, {
-//   ForwardedRef,
-//   forwardRef,
-//   ForwardRefRenderFunction,
-//   PropsWithChildren,
-//   useMemo,
-// } from 'react';
+// import React, {ForwardedRef} from 'react';
 // import type {FC, ComponentProps} from 'react';
 // import {TextInput as RNTextInput} from 'react-native';
 // import type {StyleProp, TextStyle} from 'react-native';
 // import {useTheme} from 'react-native-paper';
 
-// // export type TextInputProps = ComponentProps<typeof RNTextInput> & {
-// //   ref: ForwardedRef<RNTextInput>;
-// // };
-// export type TextInputProps = ComponentProps<typeof RNTextInput>;
-
-// const _TextInput: ForwardRefRenderFunction<RNTextInput, TextInputProps> = (
-//   {style, ...props},
-//   ref,
-// ) => {
+// export type TextInputProps = ComponentProps<typeof RNTextInput> & {
+//   ref: ForwardedRef<RNTextInput>;
+// };
+// export const TextInput: FC<TextInputProps> = ({style, ref, ...props}) => {
 //   const {colors} = useTheme();
 
 //   return (
@@ -30,7 +19,7 @@
 //     />
 //   );
 // };
-// export const TextInput = forwardRef(_TextInput);
+
 // 최종 모습
 import React, {forwardRef} from 'react';
 import type {ForwardRefRenderFunction, ComponentProps} from 'react';
