3753e840e2df082b6a042ec5c705f64d2ac02284
rrilla <rrilla@naver.com>
2021-06-13 23:22:07 +0900

    내비게이션에 애니메이션 적용

--- ch07/ch07_2/src/screens/MainNavigator.tsx
+++ ch07/ch07_2/src/screens/MainNavigator.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {Colors} from 'react-native-paper';
+import type {StackNavigationOptions} from '@react-navigation/stack';
+import {useNavigationHorizontalInterpolator} from '../hooks';
 import Home from './Home';
 import HomeLeft from './HomeLeft';
 import HomeRight from './HomeRight';
@@ -8,23 +9,35 @@ import HomeRight from './HomeRight';
 const Stack = createStackNavigator();
 
 // prettier-ignore
-export default function MainNavigator(){
+export default function MainNavigator() {
+  const interpolator = useNavigationHorizontalInterpolator();
+  const leftOptions = useMemo<StackNavigationOptions>(
+    () => ({
+      gestureDirection: 'horizontal-inverted',
+      cardStyleInterpolator: interpolator,
+    }),
+    [],
+  );
+  const rightOptions = useMemo<StackNavigationOptions>(
+    () => ({
+      gestureDirection: 'horizontal',
+      cardStyleInterpolator: interpolator,
+    }),
+    [],
+  );
   return (
-    <Stack.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        headerShown:false,
-        headerStyle: {
-          backgroundColor: Colors.pink500,
-        },
-        headerTintColor: 'white',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="HomeLeft" component={HomeLeft} />
-      <Stack.Screen name="HomeRight" component={HomeRight} />
+      <Stack.Screen
+        name="HomeLeft"
+        component={HomeLeft}
+        options={leftOptions}
+      />
+      <Stack.Screen
+        name="HomeRight"
+        component={HomeRight}
+        options={rightOptions}
+      />
     </Stack.Navigator>
   );
 }
