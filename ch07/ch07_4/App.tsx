d3e73b5daf35a5dc389890edab345426b0772db2
rrilla <rrilla@naver.com>
2021-06-19 16:27:12 +0900

    드로어 내비게이션 - 프로젝트 셋팅

---
+++ ch07/ch07_4/App.tsx
@@ -0,0 +1,34 @@
+import React, {useCallback, useState} from 'react';
+import 'react-native-gesture-handler';
+import {enableScreens} from 'react-native-screens';
+import {SafeAreaProvider} from 'react-native-safe-area-context'; //아이폰 화면문제
+// prettier-ignore
+import {DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import MainNavigator from './src/screens/MainNavigator';
+import {AppearanceProvider, useColorScheme} from 'react-native-appearance';
+import {ToggleThemeProvider} from './src/contexts';
+
+enableScreens(); //react-native-screens 모듈 동작에 필요한 코드
+
+export default function App() {
+  const scheme = useColorScheme();
+  const [theme, setTheme] = useState(
+    scheme === 'dark' ? DarkTheme : DefaultTheme,
+  );
+  const toggleTheme = useCallback(
+    () => setTheme(({dark}) => (dark ? DefaultTheme : DarkTheme)),
+    [],
+  );
+
+  return (
+    <AppearanceProvider>
+      <ToggleThemeProvider toggleTheme={toggleTheme}>
+        <SafeAreaProvider>
+          <NavigationContainer theme={theme}>
+            <MainNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </ToggleThemeProvider>
+    </AppearanceProvider>
+  );
+}
